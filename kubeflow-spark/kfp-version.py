64f009894fb3764f9853c595fcb999c55ee2cf8c
DieWay <a0987974634@gmail.com>
2024-08-20 00:40:23 +0800

    spark pipeline on kfpv1 from sbakiu's repo

--- kubeflow-spark/kfp-version.py
+++
@@ -1,5 +0,0 @@
-import kfp
-from kfp import dsl
-
-if __name__ == "__main__":
-    print(kfp.__version__)
--- kubeflow-spark/scripts/start-minikube.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-minikube start  --memory 8192 --cpus 4 --driver=docker --kubernetes-version=v1.22.9
-minikube addons enable metrics-server
-#minikube addons enable registry
-#minikube addons enable ingress
