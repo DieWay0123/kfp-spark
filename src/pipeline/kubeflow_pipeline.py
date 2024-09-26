72cb7265d22a0e3a91de2ac531c0e0b04943aae9
DieWay <a0987974634@gmail.com>
2024-09-26 14:12:59 +0800

    finish process pipeline but pyspark broke

--- src/pipeline/kubeflow_pipeline.py
+++ src/pipeline/kubeflow_pipeline.py
@@ -1,83 +1,80 @@
 import json
 import time
+
+from yaml import YAMLObject
 import yaml
 
 import kfp.components as comp
 import kfp.dsl as dsl
+from kfp.dsl import container_component, ContainerSpec, Input, Output, InputPath, OutputPath, Artifact, Dataset
+from kfp import kubernetes
 
-SPARK_COMPLETED_STATE = "COMPLETED"
-SPARK_APPLICATION_KIND = "sparkapplications"
 
-
-def get_spark_job_definition(sparkApplication_NS, sparkApplication_SA):
+def get_spark_job_definition(dataset_path):
     """
     Read Spark Operator job manifest file and return the corresponding dictionary and
     add some randomness in the job name
     :return: dictionary defining the spark job
     """
     # Read manifest file
-    with open("spark-job-python-10kprocess.yaml", "r") as stream:
+    with open('spark-job-python-10kprocess.yaml', "r") as stream:
         spark_job_manifest = yaml.safe_load(stream)
 
     # Add epoch time in the job name
     epoch = int(time.time())
     spark_job_manifest["metadata"]["name"] = spark_job_manifest["metadata"]["name"].format(epoch=epoch)
-    spark_job_manifest["metadata"]["namespace"] = spark_job_manifest["metadata"]["namespace"].format(NS=sparkApplication_NS)
-    spark_job_manifest["spec"]["driver"]["serviceAccount"] = spark_job_manifest["spec"]["driver"]["serviceAccount"].format(SA=sparkApplication_SA)
-
     return spark_job_manifest
 
+#Get and concat dataset
+@dsl.component(
+  base_image="dieway/get-dataset-nfs-server:diabetes",
+  packages_to_install=["python-dotenv==1.0.1", "pandas==2.2.2", "numpy==2.0.0"]
+)
+def load_raw_datasets_from_nfs(
+  diabetes_dataset: Output[Dataset]
+):
+  import pandas as pd
+# fix csv path
+  dataset_pd = pd.read_csv('/home/nfs/10kdataset.csv')
+  dataset_pd.to_csv(diabetes_dataset.path)
 
+@dsl.component(
+  base_image="python:3.9"
+)
 def print_msg(msg: str) -> str:
     print(msg)
     return msg
 
 
-@dsl.graph_component  # Graph component decorator is used to annotate recursive functions
-def graph_component_spark_app_status(input_application_name):
-    k8s_get_op = comp.load_component_from_file("k8s-get-component.yaml")
-    check_spark_application_status_op = k8s_get_op(
-        name=input_application_name,
-        kind=SPARK_APPLICATION_KIND
-    )
-    # Remove cache
-    check_spark_application_status_op.execution_options.caching_strategy.max_cache_staleness = "P0D"
-
-    time.sleep(5)
-    with dsl.Condition(check_spark_application_status_op.outputs["applicationstate"] != SPARK_COMPLETED_STATE):
-        graph_component_spark_app_status(check_spark_application_status_op.outputs["name"])
-
 @dsl.pipeline(
     name="Spark Operator job pipeline",
     description="Spark Operator job pipeline"
 )
-def spark_job_pipeline(
-  sparkApplication_NS: str,
-  sparkApplication_SA: str,
-):
+def spark_job_pipeline():
 
-    # Load spark job manifest
-    spark_job_definition = get_spark_job_definition(sparkApplication_NS=sparkApplication_NS, sparkApplication_SA=sparkApplication_SA)
-
-    # Load the kubernetes apply component
+    load_raw_data_from_nfs_task = load_raw_datasets_from_nfs()
+    load_raw_data_from_nfs_task.set_caching_options(enable_caching=False)
+    kubernetes.mount_pvc(
+    	load_raw_data_from_nfs_task,
+    	pvc_name='dataset-pvc',
+    	mount_path="/home/nfs"
+    )
+    
+    dataset_path = load_raw_data_from_nfs_task.outputs['diabetes_dataset']
+    spark_job_definition = get_spark_job_definition(dataset_path=dataset_path)
     k8s_apply_op = comp.load_component_from_file("k8s-apply-component.yaml")
-
-    # Execute the apply command
-    spark_job_op = k8s_apply_op(object=json.dumps(spark_job_definition))
+    spark_job_task = k8s_apply_op(object=json.dumps(spark_job_definition), dataset=dataset_path)
 
     # Fetch spark job name
-    spark_job_name = spark_job_op.outputs["name"]
+    spark_job_name = spark_job_definition["metadata"]["name"]
+    spark_job_namespace = spark_job_definition["metadata"]["namespace"]
+    spark_job_task.set_caching_options(enable_caching=False)
     
-    spark_job_op.execution_options.caching_strategy.max_cache_staleness = "P0D"
+    check_sparkapplication_status_op = comp.load_component_from_file("checkSparkapplication.yaml")
+    check_sparkapplication_status_task = check_sparkapplication_status_op(name=spark_job_name, namespace=spark_job_namespace).after(spark_job_task)
 
-    print_op = comp.func_to_container_op(
-      print_msg,
-      base_image="python:3.9",
-    )
-
-    print_message = print_op(f"Job {spark_job_name} is completed.")
-    #print_message.after(spark_application_status_op)
-    print_message.execution_options.caching_strategy.max_cache_staleness = "P0D"
+    print_message_task = print_msg(msg=f"Job {spark_job_name} is completed.").after(check_sparkapplication_status_task)
+    print_message_task.set_caching_options(enable_caching=False)
 
 
 if __name__ == "__main__":
@@ -86,6 +83,6 @@ if __name__ == "__main__":
     import logging
     logging.basicConfig(level=logging.INFO)
     pipeline_func = spark_job_pipeline
-    pipeline_filename = pipeline_func.__name__ + ".yaml"
+    pipeline_filename = "sparkJobPipeline.yaml"
     compiler.Compiler().compile(pipeline_func, pipeline_filename)
     logging.info(f"Generated pipeline file: {pipeline_filename}.")
